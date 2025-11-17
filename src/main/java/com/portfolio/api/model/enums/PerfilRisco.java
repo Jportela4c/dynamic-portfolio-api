fefde977212fb08ca1033af9c5469e6b8e1e2d09
jportela4c <jportela4c@gmail.com>
2025-11-17 01:00:23 -0300

    refactor: add @JsonProperty to enums for API consistency

--- src/main/java/com/portfolio/api/model/enums/PerfilRisco.java
+++ src/main/java/com/portfolio/api/model/enums/PerfilRisco.java
@@ -1,16 +1,20 @@
 package com.portfolio.api.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Perfil de risco do investidor")
 public enum PerfilRisco {
 
     @Schema(description = "Investidor conservador - prioriza segurança e liquidez")
-    Conservador,
+    @JsonProperty("Conservador")
+    CONSERVADOR,
 
     @Schema(description = "Investidor moderado - equilibra rentabilidade e risco")
-    Moderado,
+    @JsonProperty("Moderado")
+    MODERADO,
 
     @Schema(description = "Investidor agressivo - busca alta rentabilidade aceitando mais risco")
-    Agressivo
+    @JsonProperty("Agressivo")
+    AGRESSIVO
 }
--- src/main/java/com/portfolio/api/model/enums/TipoProduto.java
+++ src/main/java/com/portfolio/api/model/enums/TipoProduto.java
@@ -1,31 +1,40 @@
 package com.portfolio.api.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Tipo de produto financeiro disponível")
 public enum TipoProduto {
 
     @Schema(description = "Certificado de Depósito Bancário")
+    @JsonProperty("CDB")
     CDB,
 
     @Schema(description = "Letra de Crédito Imobiliário (isento de IR)")
+    @JsonProperty("LCI")
     LCI,
 
     @Schema(description = "Letra de Crédito do Agronegócio (isento de IR)")
+    @JsonProperty("LCA")
     LCA,
 
     @Schema(description = "Títulos públicos do governo federal")
+    @JsonProperty("TesouroDireto")
     TESOURO_DIRETO,
 
     @Schema(description = "Fundo de investimento em renda fixa")
+    @JsonProperty("FundoRendaFixa")
     FUNDO_RENDA_FIXA,
 
     @Schema(description = "Fundo de investimento multimercado")
+    @JsonProperty("FundoMultimercado")
     FUNDO_MULTIMERCADO,
 
     @Schema(description = "Fundo de investimento em ações")
+    @JsonProperty("FundoAcoes")
     FUNDO_ACOES,
 
     @Schema(description = "Fundo de Investimento Imobiliário")
+    @JsonProperty("FII")
     FII
 }
