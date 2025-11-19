a440f57fb2834987dcd13bb7cc2f2577a5cc4781
jportela4c <jportela4c@gmail.com>
2025-11-18 23:59:16 -0300

    test: add @WithAnonymousUser for unauthorized test

--- src/test/java/com/portfolio/api/integration/SimulationIntegrationTest.java
+++ src/test/java/com/portfolio/api/integration/SimulationIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -88,6 +89,7 @@ class SimulationIntegrationTest {
     }
 
     @Test
+    @WithAnonymousUser
     void shouldReturnUnauthorizedWithoutToken() throws Exception {
         SimulationRequest request = new SimulationRequest();
         request.setClienteId(123L);
