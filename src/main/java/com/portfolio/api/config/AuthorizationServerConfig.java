8bf269f223cd5d1e07353af5404393780d4c200e
jportela4c <jportela4c@gmail.com>
2025-11-20 16:11:27 -0300

    feat: add form login security filter chain

--- src/main/java/com/portfolio/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/portfolio/api/config/AuthorizationServerConfig.java
@@ -74,6 +74,19 @@ public class AuthorizationServerConfig {
         return http.build();
     }
 
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
     /**
      * Configures single OAuth2 client for portfolio web application.
      * Uses Authorization Code flow with username/password authentication.
